667aaffbbaa37f31a256a88c062aef602cceab69
ThanuraSilva <thanurasilva1989@gmail.com>
2026-02-22 13:25:40 +0530

    Get form data - Get event and add data to FormData object

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello CMJD")
+const form = document.getElementById("data-form")
+let courseData = {}
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    //Get the form data
+   const dataForm =  new FormData(form)
+
+   const firstName = dataForm.get("first-name");
+   const lastName = dataForm.get("last-name");
+   const batch = dataForm.get("batch");
+   const regNo = dataForm.get("regNo");
+   const course = dataForm.get("course");
+   const email = dataForm.get("email");
+
+})
+
+
+
